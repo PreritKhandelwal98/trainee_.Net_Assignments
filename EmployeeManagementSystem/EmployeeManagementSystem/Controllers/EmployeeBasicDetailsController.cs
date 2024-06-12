4c1e4338d56664a199d73fd6cfd046d986f2ab73
Prerit Khandelwal <prerit.web@gmail.com>
2024-06-12 19:57:09 +0530

    create controller

--- EmployeeManagementSystem/EmployeeManagementSystem/Controllers/EmployeeBasicDetailsController.cs
+++ EmployeeManagementSystem/EmployeeManagementSystem/Controllers/EmployeeBasicDetailsController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeManagementSystem.DTO;
+using EmployeeManagementSystem.Interface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagementSystem.Controllers
 {
+    [Route("api/[controller]/[action]")]
+    [ApiController]
     public class EmployeeBasicDetailsController : Controller
     {
-        public IActionResult Index()
+        private readonly IEmployeeBasicDetailsService _basicDetailService;
+
+        public EmployeeBasicDetailsController(IEmployeeBasicDetailsService basicDetailService)
         {
-            return View();
+            _basicDetailService = basicDetailService;
+        }
+
+        [HttpPost]
+        public async Task<EmployeeBasicDetailsDTO> AddBasicDetail(EmployeeBasicDetailsDTO basicDetailsDTO)
+        {
+            return await _basicDetailService.AddEmployeeBasicDetails(basicDetailsDTO);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<EmployeeBasicDetailsDTO>> GetAllEmployeeBasicDetails()
+        {
+            return await _basicDetailService.GetAllEmployeeBasicDetails();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<EmployeeBasicDetailsDTO> GetEmployeeBasicDetailsById(string id)
+        {
+            return await _basicDetailService.GetEmployeeBasicDetailsById(id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployeeBasicDetails(string id, EmployeeBasicDetailsDTO basicDetailsDTO)
+        {
+            try
+            {
+                var updatedEmployee = await _basicDetailService.UpdateEmployeeBasicDetails(id, basicDetailsDTO);
+                return Ok(updatedEmployee);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in UpdateVisitor (Controller): {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployeeBasiclDetails(string id)
+        {
+            await _basicDetailService.DeleteEmployeeBasicDetails(id);
+            return NoContent();
         }
     }
 }
